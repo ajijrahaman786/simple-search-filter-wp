13199115cd94c64db33544ef64dc11c9bb57fb1a
Ajij Rahaman <ajij@appycodes.com>
2023-09-01 18:46:45 +0530

    wptsf

---
+++ admin/wptsf.php
@@ -0,0 +1,13 @@
+
+    <h2>Ice Cream Collection</h2>
+    <div class="search-bar">
+        <input type="text" placeholder="Search" id="search-bar" value="">
+    </div>
+    <table id="table-wrapper">
+        <thead id="table-header-row">
+
+        </thead>
+        <tbody id="table-body">
+
+        </tbody>
+    </table>
--- simple-search-filter.php
+++ simple-search-filter.php
@@ -1,8 +1,8 @@
 <?php 
 /*
-Plugin Name:    simple search filter
-PLugin URI :    https://wordpress.org/plugins/simple-search-filter/
-Description:    Currency calculator, converts amounts between currencies.
+Plugin Name:    Simple table of Search filter
+PLugin URI :    https://wordpress.org/plugins/simple-table-of-search-filter/
+Description:    The most universal filters plugin for filtering any data of the table.
 Version:        1.0.0
 Requires at least: 5.2
 Requires PHP:   7.2
@@ -10,43 +10,89 @@ Author:         Ajij Rahaman
 Author URI:     https://ajijrahaman.com/
 Update URI:     https://github.com/ajijrahaman786
 License:        GPLv2 or later
-Text Domain:    ssf
+Text Domain:    wptsf
 */
 
-// inlcuding css
-add_action('wp_enqueue_scripts', 'enqueue_plugin_styles', 10); 
-function enqueue_plugin_styles() {
-    wp_enqueue_style('sttt-style', plugin_dir_url(__FILE__) . 'css/sttt-style.css', array());
+// inlcuding css for the site
+add_action('wp_enqueue_scripts', 'enqueue_styles_of_site', 10); 
+function enqueue_styles_of_site() {
+    wp_enqueue_style('wptsf-style', plugin_dir_url(__FILE__) . 'css/wptsf-style.css', array());
 }
-// including javaScript
-add_action('wp_enqueue_scripts', 'enqueue_plugin_scripts', 50);
-function enqueue_plugin_scripts(){
-  wp_enqueue_script('sttt-script', plugin_dir_url(__FILE__) . 'js/sttt-plugin.js', array(), true);
+// including javaScript for the site
+add_action('wp_enqueue_scripts', 'enqueue_scripts_of_site', 50);
+function enqueue_scripts_of_site(){
+  wp_enqueue_script('wptsf-script', plugin_dir_url(__FILE__) . 'js/wptsf-plugin.js', array(), true);
+}
+// including javaScript for the plugin settings
+add_action('admin_footer', 'enqueue_scripts_of_plugin_settings', 50);
+function enqueue_scripts_of_plugin_settings(){
+  wp_enqueue_script('wptsf-script', plugin_dir_url(__FILE__) . 'js/wptsf-plugin.js', array(), true);
+}
+// inlcuding css for the plugin settings
+add_action('admin_footer', 'enqueue_styles_of_plugin_settings', 10); 
+function enqueue_styles_of_plugin_settings() {
+    wp_enqueue_style('wptsf-style', plugin_dir_url(__FILE__) . 'admin/css/wptsf-style.css', array());
 }
 
-// js plugin setting Activation
-
-function sttt_tic_tac_toe(){
-    ?>
-
-<h2>Ice Cream Collection</h2>
-<div class="search-bar">
-    <input type="text" placeholder="Search" id="search-bar" value="">
-    </div>
-<table id="table-wrapper">
-  <thead id="table-header-row">
-
-  </thead>
-   <tbody id="table-body">
-
-   </tbody>
-</table>
+// plugin setting Activation
+add_action("wp_head", "TSF_page_html", 10);
+function TSF_page_html(){
+  include_once "admin/wptsf.php";
+  ?>
 
     
 <?php
 }
 
-add_action("wp_head", "sttt_tic_tac_toe", 30);
+
+function TSF_main_settings_html(){
+  ?>
+  <div class="wrap">
+		<h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
+		<form action="options.php" method="post">
+			<?php
+			// output security fields for the registered setting "wptsf_settings"
+			settings_fields( 'wptsf_settings' );
+			// output setting sections and their fields
+			// (sections are registered for "wptsf_settings", each field is registered to a specific section)
+			do_settings_sections( 'wptsf_settings' );
+			// output save settings button
+			submit_button( 'Save Settings' );
+			?>
+		</form>
+	</div>
+  <?php
+
+}
+
+
+// register wptsf_register_menu_page to the admin_init action hook
+add_action('admin_menu', 'wptsf_register_menu_page');
+function wptsf_register_menu_page(){
+
+  add_menu_page("Table of Search Filter WP", "TSF", "manage_options", "tsf-settings", "TSF_page_html", "dashicons-editor-table", 20);
+
+  add_submenu_page("tsf-settings", "TSF Settings", "Settings", "manage_options", "settings", "TSF_main_settings_html", 10);
+
+}
+
+
+// register wptsf_settings_init to the admin_init action hook
+add_action('admin_init', 'wptsf_settings_init');
+function wptsf_settings_init(){
+  // Register a new setting for "wptsf" page.
+	register_setting( 'wptsf_settings', 'wptsf_table', "wptsf_table_cb");
+  // Register a new section in the "wptsf" page.
+	add_settings_section("wptsf_settings_section", "wptsf_table_section", "wptsf_table_section_cb", "tsf-settings");
+  // Register a new field in the "wptsf" page.
+  add_settings_field("wptsf_settings_field", "wptsf_table_field", "wptsf_table_label_cb", "TSF","wptsf_settings_section");
+
+}
+
+
+
+
+
 
 
 ?>
